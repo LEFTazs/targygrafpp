dd78a11b465c9731632f6727d216c034edb42211
Szabó Krisztián <szabokrisy444@gmail.com>
2020-04-22 17:51:16 +0200

    Add basic frontend

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -6,17 +6,48 @@ function initalizePage() {
     subjects = backendGet("/getsubjects");
     console.log(subjects);
     $("#subjects").empty();
+    //find semester count
+    var semestermax = 0;
     $.each(subjects, function (i, data) {
-        $("#subjects").append("Név: " + data.name + ", ");
-        $("#subjects").append("Kód: " + data.code + ", ");
-        $("#subjects").append("Kreditérték: " + data.creditValue + ", ");
-        $("#subjects").append("<br>     Előfeltételek kódjai: ");
-        $.each(data.prerequisites, function (j, prerequisite) {
-            $("#subjects").append(prerequisite + ", ");
-        });
-        $("#subjects").append("<br>");
+        if(data.semester > semestermax){
+            semestermax = data.semester;
+        }
+    });
+    console.log(semestermax);
+    //semesters setup
+    for(i = 1; i <= semestermax;i++){
+      $("#subjects").append("<div class='semesterblock'><strong>"+ i +".félév</strong></div>");
+    }
+    //load subjects to semesters
+    for(i = 0; i < subjects.length; i++){
+        $(".semesterblock:nth-of-type(" +subjects[i].semester + ")").append(
+                "<div class='subject' id='"+ subjects[i].code +"'><div class='subjectname'>"+ subjects[i].name +"</div><div class='subjectcode'>"+ subjects[i].code +"</div><div class='subjectcredit'>"+ subjects[i].creditValue +"</div></div>");
+    }
+
+    $(".subject").click(function(event){
+      var clickedSubject;
+        if($(this).hasClass("subject")){
+          clickedSubject = this.id;
+        }else{
+          clickedSubject = this.parent().id;
+        }
+
+      $("#"+clickedSubject).css("border-color","red");
+      for(i = 0; i <= semestermax;i++){
+        for(y= 0; y < subjects[i].prerequisites.length; y++){
+            console.log(subjects[i].prerequisites[y]);
+            console.log(clickedSubject);
+          if(subjects[i].prerequisites[y] == clickedSubject){
+            console.log(subjects[i].prerequisites[y]);
+            $("#"+subjects[i].code).css("background-color","green");
+          }
+        }
+      }
+
     });
 }
+
+
 
 function backendGet(endpoint) {
     var url = "http://" + window.location.host + "/" + endpoint;
@@ -36,7 +67,7 @@ function backendGet(endpoint) {
     });
     return response;
 }
- 
+
 function backendPost(endpoint, json) {
     var url = "http://" + window.location.host + "/" + endpoint;
 
